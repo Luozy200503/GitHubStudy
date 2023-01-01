cf7e5a39bf10e2787c4d6fa486ba7cdad445454e
Luozy200503 <lzy15071905869@163.com>
2023-01-01 15:14:27 +0800

    GitHub第一次修改

--- GitHubTest/main.cpp
+++ GitHubTest/main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
+#include"test.h"
 using namespace std;
 int main()
 {
+	sum(3, 5);
 	std::cout << "hello,world!" << "\n";
 }
---
+++ GitHubTest/test.cpp
@@ -0,0 +1,5 @@
+#include"test.h"
+void sum(int a, int b)
+{
+	cout << a + b << "\n";
+}
